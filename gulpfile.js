914705ed05d6b18e00a26159cd46844f83c754aa
Guillaume Perot <guillaume.perot@360learning.com>
2015-08-18 12:49:37 +0200

    add gulp file to the project and modified a param.json

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require("gulp");
+var plugins = require("gulp-load-plugins")();
+var runSequence = require("run-sequence");
+
+var jsConcatFiles = [
+    'js/cookie.js',
+    'js/tools.js',
+    'js/crazyEgg.js',
+    'js/nicePopup.js',
+    'js/lang/popupLanguage.js',
+    'js/popup.js',
+    'js/animations.js',
+    'js/browser.js',
+    'js/triggerPopup.js',
+    'js/tree.js',
+    'js/menu.js',
+    'js/footer.js',
+    'js/googleSlide.js',
+    'js/toggle.js',
+    'js/index.js'
+];
+
+gulp.task("scripts:dev", function () {
+    return gulp.src(jsConcatFiles)
+        .pipe(plugins.concat("concat.js"))
+        .pipe(gulp.dest("js/min"));
+});
+
+gulp.task("scripts:prod", ["scripts:dev"], function () {
+    return gulp.src("js/min/concat.js")
+        .pipe(plugins.plumber())
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename("common.min.js"))
+        .pipe(gulp.dest("js/min"));
+});
+
+gulp.task("styles:dev", function () {
+    return gulp.src("styles/sass/*.scss")
+        .pipe(plugins.plumber())
+        .pipe(plugins.compass({
+            style: "expanded",
+            comments: true,
+            css: "styles/css",
+            sass: "styles/sass",
+            environment: "development"
+        }))
+        .pipe(gulp.dest("styles/css"));
+});
+
+gulp.task("styles:prod", function () {
+    return gulp.src("styles/sass/*.scss")
+        .pipe(plugins.plumber())
+        .pipe(plugins.compass({
+            style: "compressed",
+            comments: false,
+            css: "styles/css",
+            sass: "styles/sass",
+            environment: "production"
+        }))
+        .pipe(gulp.dest("styles/css"));
+});
+
+gulp.task("default", function () {
+    gulp.watch(jsConcatFiles, ["scripts:dev"]);
+
+    gulp.watch("styles/sass/*.scss")
+        .on("change", function (event) {
+            gulp.src(event.path)
+                .pipe(plugins.plumber())
+                .pipe(plugins.compass({
+                    style: "expanded",
+                    comments: true,
+                    css: "styles/css",
+                    sass: "styles/sass",
+                    environment: "development"
+                }))
+                .pipe(gulp.dest("styles/css"));
+        });
+});
+
+gulp.task("build", function (cb) {
+    runSequence(["scripts:dev", "styles:dev"], cb);
+});
+
+gulp.task("build:prod", function (cb) {
+    runSequence(["scripts:prod", "styles:prod"], cb);
+});
